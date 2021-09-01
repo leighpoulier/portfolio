1094bb9c7d4cfd661a14dcdb352a285d101ee82c
leighpoulier <86460742+leighpoulier@users.noreply.github.com>
2021-09-02 00:33:41 +1000

    updated responsivemenu sandbox, completed functionality of responsive navigation button/menu across 3 form factors

--- sandboxes/08-responsivemenu/js/script.js
+++ sandboxes/08-responsivemenu/js/script.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
 
     document.getElementById('hamburger').addEventListener('click', hamburgerToggle);
-
     document.getElementById('close-button').addEventListener('click', hamburgerToggle);
+    document.getElementById('nav-menu-modal-overlay').addEventListener('click', hamburgerToggle);
 
     function hamburgerToggle(event) {
-        nav = document.getElementById('nav');
+        nav = document.getElementById('nav-container');
         if (nav.classList.contains("nav-up")) {
             navOpen(nav);
         } else {
@@ -14,15 +14,17 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     function navOpen(element) {
-        element.style.top = 0;
+        // element.style.top = 0;
         // element.style.display = "block";
         element.classList.replace("nav-up", "nav-down");
+        element.classList.replace("nav-transparent", "nav-opaque");
     }
 
     function navClose(element) {
-        element.classList.replace("nav-down", "nav-up");
+        element.classList.replace("nav-opaque", "nav-transparent");
         setTimeout(function() {
-            element.style.top = "-100%";
+            // element.style.top = "-100%";
+            element.classList.replace("nav-down", "nav-up");
             // element.style.display = "none";
         }, 500); // timed to match nav transition duration;
 
